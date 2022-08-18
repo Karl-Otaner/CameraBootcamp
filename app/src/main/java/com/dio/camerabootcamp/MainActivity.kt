ac3f076588f01f0c5d234c6357fcbac12f183ba2
Karl Otaner <carlosrnsilva01@gmail.com>
2022-08-18 00:36:00 -0300

    Projeto Finalizado

--- app/src/main/java/com/dio/camerabootcamp/MainActivity.kt
+++ app/src/main/java/com/dio/camerabootcamp/MainActivity.kt
@@ -2,17 +2,20 @@ package com.dio.camerabootcamp
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-
 class MainActivity : AppCompatActivity() {
+    var image_uri: Uri? = null
 
 
     companion object {
@@ -20,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         private val IMAGE_PICK_CODE = 1001
 
         private val PERMISSION_CODE_CAMERA_CAPTURE = 2000
-
+        private val OPEN_CAMERA_CODE = 2001
     }
 
 
@@ -32,15 +35,14 @@ class MainActivity : AppCompatActivity() {
         pick_button.setOnClickListener {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-                    == PackageManager.PERMISSION_DENIED) {
+                    == PackageManager.PERMISSION_DENIED
+                ) {
                     val permission = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
                     requestPermissions(permission, PERMISSION_CODE_IMAGE_PICK)
-                }
-                else {
+                } else {
                     pickImageFronGalery()
                 }
-            }
-            else {
+            } else {
                 pickImageFronGalery()
             }
         }
@@ -48,19 +50,22 @@ class MainActivity : AppCompatActivity() {
         open_camera_button.setOnClickListener {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 if (checkSelfPermission(Manifest.permission.CAMERA)
-                 == PackageManager.PERMISSION_DENIED ||
-                    checkSelfPermission(Manifest.permission
-                    .WRITE_EXTERNAL_STORAGE)
-                        == PackageManager.PERMISSION_DENIED) {
-                    val permissions = arrayOf(Manifest.permission.CAMERA,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_DENIED ||
+                    checkSelfPermission(
+                        Manifest.permission
+                            .WRITE_EXTERNAL_STORAGE
+                    )
+                    == PackageManager.PERMISSION_DENIED
+                ) {
+                    val permissions = arrayOf(
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
                     requestPermissions(permissions, PERMISSION_CODE_CAMERA_CAPTURE)
-                }
-                else {
+                } else {
                     openCamera()
                 }
-            }
-            else {
+            } else {
                 openCamera()
 
             }
@@ -71,25 +76,25 @@ class MainActivity : AppCompatActivity() {
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
-        grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults) //Ele não usou!
-        when(requestCode) {
+        grantResults: IntArray
+    ) {
+       super.onRequestPermissionsResult(requestCode, permissions, grantResults) //O prof. não usou!, se eu não usar tomo um erro
+        when (requestCode) {
             PERMISSION_CODE_IMAGE_PICK -> {
-                if (grantResults.size >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     pickImageFronGalery()
-                }
-                else {
+                } else {
                     Toast.makeText(this, "Permissão Negada", Toast.LENGTH_SHORT).show()
 
                 }
             }
             PERMISSION_CODE_CAMERA_CAPTURE -> {
-                if (grantResults.size >1 &&
-                        grantResults[0] == PackageManager.PERMISSION_GRANTED &&
-                        grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                if (grantResults.size > 1 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED
+                ) {
                     openCamera()
-                }
-                else {
+                } else {
                     Toast.makeText(this, "Permissão Negada", Toast.LENGTH_SHORT).show()
                 }
             }
@@ -102,10 +107,25 @@ class MainActivity : AppCompatActivity() {
         startActivityForResult(intent, IMAGE_PICK_CODE)
     }
 
+    private fun openCamera() {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "Nova foto")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Imagem capturada pela camera")
+        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
+
+        startActivityForResult(cameraIntent, OPEN_CAMERA_CODE)
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
             image_view.setImageURI(data?.data)
+        }
+        if (resultCode == RESULT_OK && requestCode == OPEN_CAMERA_CODE) {
+            image_view.setImageURI(image_uri)
         }
     }
 
